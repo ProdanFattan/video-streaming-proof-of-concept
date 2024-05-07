5d4e9f3d044c79d116c49f8e024617a8e329dc80
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-05-07 21:17:06 +0600

    cors and access middleware

--- server/index.js
+++ server/index.js
@@ -6,6 +6,23 @@ import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+// cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", function (req, res) {
   res.json({ message: "Hello World..." });
 });
